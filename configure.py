50bab24a74007435ed259de4007742da1ff5c1fe
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-20 14:41:27 -0800

    updates

--- configure.py
+++ configure.py
@@ -6,10 +6,17 @@ Configure R36S-Bioinformatics
 # imports
 from subprocess import run
 
+# pull the latest updates from GitHub
+def pull_latest():
+    command = ['git', 'pull']
+    print("Checking for updates...")
+    proc = run(command, capture_output=True)
+    print(proc.returncode)
+
 # main program logic
 def main():
     print("Hello, world!")
-    run(['git', 'pull'])
+    pull_latest()
 
 # run program
 if __name__ == "__main__":
