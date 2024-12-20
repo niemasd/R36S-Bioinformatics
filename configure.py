4eb9b1d607b251ce49d4f2910b978a33db9c0892
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-20 14:43:28 -0800

    updates

--- configure.py
+++ configure.py
@@ -6,16 +6,21 @@ Configure R36S-Bioinformatics
 # imports
 from subprocess import run
 
+# print an error message and exit
+def error(s='', end='\n', returncode=1):
+    print(s, end=end); exit(returncode)
+
 # pull the latest updates from GitHub
 def pull_latest():
     command = ['git', 'pull']
     print("Checking for updates...")
     proc = run(command, capture_output=True)
+    if proc.returncode != 0:
+        error("Failed to check for updates via `git pull`. Make sure your R36S has internet connection.")
     print(proc.returncode)
 
 # main program logic
 def main():
-    print("Hello, world!")
     pull_latest()
 
 # run program
