f7adaa7ac4e4793be4598f25e0f819fbaca98302
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-20 16:54:35 -0800

    updates

--- apps/configure.sh
+++ apps/configure.sh
@@ -3,7 +3,7 @@ sudo rg351p-js2xbox --silent -t oga_joypad &
 sudo ln -s /dev/input/event3 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
 sudo chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
 echo "Finding configuration script in home directory (~)..."
-find ~ -name "configure.sh"
+find ~ -name "configure.py"
 sleep 2
 sudo kill $(pidof rg351p-js2xbox)
 sudo rm /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
