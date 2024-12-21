e9453b00351a1d5989774d3e4e4142b6c1b5802a
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-20 17:00:44 -0800

    updates

--- apps/configure.sh
+++ apps/configure.sh
@@ -3,7 +3,7 @@ sudo rg351p-js2xbox --silent -t oga_joypad &
 sudo ln -s /dev/input/event3 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
 sudo chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
 echo "Finding configuration script in home directory (~)..."
-CONFIG_PY=$(find ~ -name 'configure.py' 2> /dev/null | grep 'hiR36S-Bioinformatics/configure.py' | head -1)
+CONFIG_PY=$(find ~ -name 'configure.py' 2> /dev/null | grep 'R36S-Bioinformatics/configure.py' | head -1)
 if [[ -z "${CONFIG_PY}" ]] ; then
     echo "ERROR: R36S-Bioinformatics/configure.py not found"
     sleep 5
