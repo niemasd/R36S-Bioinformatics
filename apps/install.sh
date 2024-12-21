f5acdba955e18144ccced398e8df5ab9da21d97c
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-21 09:16:00 -0800

    updates

--- apps/install.sh
+++ apps/install.sh
@@ -13,9 +13,9 @@ if [[ -z "${INSTALL_SH}" ]] ; then
     echo "Install script not found. Cloning into home directory (~)..."
     cd ~
     git clone https://github.com/niemasd/R36S-Bioinformatics.git
-    INSTALL_SH='~/R36S-Bioinformatics/install.sh'
+    INSTALL_SH="$(realpath ~/R36S-Bioinformatics/install.sh)"
 fi
-echo "Running install script: $INSTALL_SH" && echo "" && cd $(echo $INSTALL_SH | rev | cut -d'/' -f2- | rev) && git pull && "$INSTALL_SH"
+echo "Running install script: $INSTALL_SH" && echo "" && cd "$(echo $INSTALL_SH | rev | cut -d'/' -f2- | rev)" && git pull && "$INSTALL_SH"
 
 # finish up
 KILL_PID=$(pidof rg351p-js2xbox)
