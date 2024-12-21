c23eac05f927ec9eac0923bbd6bde4768f67389f
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-20 17:16:00 -0800

    updates

--- apps/configure.sh
+++ apps/configure.sh
@@ -3,12 +3,12 @@ sudo rg351p-js2xbox --silent -t oga_joypad &
 sudo ln -s /dev/input/event3 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
 sudo chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
 echo "Finding configuration script in home directory (~)..."
-CONFIG_PY=$(find ~ -name 'configure.py' 2> /dev/null | grep 'R36S-Bioinformatics/configure.py' | head -1)
-if [[ -z "${CONFIG_PY}" ]] ; then
-    echo "ERROR: R36S-Bioinformatics/configure.py not found"
+CONFIG_SH=$(find ~ -name 'configure.sh' 2> /dev/null | grep 'R36S-Bioinformatics/configure.sh' | head -1)
+if [[ -z "${CONFIG_SH}" ]] ; then
+    echo "ERROR: R36S-Bioinformatics/configure.sh not found"
     sleep 5
 else
-    echo "Running configuration script: $CONFIG_PY" && echo "" && python3 "$CONFIG_PY"
+    echo "Running configuration script: $CONFIG_SH" && echo "" && "$CONFIG_SH"
 fi
 sudo kill $(pidof rg351p-js2xbox)
 sudo rm /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
---
+++ configure.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Configure R36S-Bioinformatics
+
+# constants
+VERSION='0.0.1'
+ES_SYSTEMS_CFG_PATH='/etc/emulationstation/es_systems.cfg'
+ES_SYSTEMS_CFG_BACKUP_PATH="$ES_SYSTEMS_CFG_PATH.r36s-bioinformatics.bak"
+DEPS_LINUX='cmake g++ git libc6-dev libsdl2-dev libsdl2-ttf-dev libstdc++-9-dev linux-libc-dev make ninja-build python3 python3-pip'
+DEPS_PYTHON='prompt_toolkit pysdl2 pysdl2-dll'
+
+# set things up
+set -e # to exit upon any failure
+sudo rg351p-js2xbox --silent -t oga_joypad &
+sudo ln -s /dev/input/event3 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+sudo chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+
+# run configuration script
+echo "=== R36S-Bioinformatics Configure v$VERSION ==="
+echo "Checking for updates to configuration script..."
+GIT_UPDATED=$(git pull | grep "Updating")
+if [[ -z "${GIT_UPDATED}" ]] ; then
+    echo "No updates available."
+else
+    echo "Updated successfully. Rerunning..."
+    bash $0
+    exit 0
+fi
+
+# finish up
+sudo kill $(pidof rg351p-js2xbox)
+sudo rm /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
