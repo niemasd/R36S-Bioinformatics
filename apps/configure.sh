1baca51007a7ada58d62d4130d41be1ca1415dee
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-20 16:51:41 -0800

    updates

--- apps/configure.sh
+++ apps/configure.sh
@@ -2,6 +2,7 @@
 sudo rg351p-js2xbox --silent -t oga_joypad &
 sudo ln -s /dev/input/event3 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
 sudo chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
-wget -qO- "https://github.com/niemasd/R36S-Bioinformatics/raw/refs/heads/main/configure.py" | python3 -
+echo "Hi, Niema!"
+sleep 5
 sudo kill $(pidof rg351p-js2xbox)
 sudo rm /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
