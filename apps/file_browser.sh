52efe84cf5ceeb12108d5b044e5502120962c3d5
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-21 15:28:05 -0800

    updates

--- apps/file_browser.sh
+++ apps/file_browser.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
 # execute file browser app
-
-# set things up
-#sudo rg351p-js2xbox --silent -t oga_joypad &
-#sudo ln -s /dev/input/event3 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
-#sudo chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
-
-# run app
-echo "Hi, Niema!"
-#SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-#python3 "$SCRIPT_DIR/file_browser.py"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo python3 "$SCRIPT_DIR/file_browser.py"
 sleep 5
-
-# finish up
-#KILL_PID=$(pidof rg351p-js2xbox)
-#if [[ ! -z "${KILL_PID}" ]] ; then
-#    sudo kill $(pidof rg351p-js2xbox)
-#fi
-#sudo rm /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
