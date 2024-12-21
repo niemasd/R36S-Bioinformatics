7dbdceaa38894cd921910eb4d113dacb704a7ccd
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-21 15:37:02 -0800

    updates

--- apps/file_browser.py
+++ apps/file_browser.py
@@ -4,10 +4,7 @@ File browser app
 '''
 from common import select_file, view_file_info
 from pathlib import Path
-from prompt_toolkit.shortcuts import message_dialog
 if __name__ == "__main__":
-    print("RUNNING FILE BROWSER APP")
-    message_dialog(title="FILE BROWSER", text="FILE BROWSER").run()
     curr_path = Path('/')
     while True:
         result = select_file(curr_path=curr_path)
