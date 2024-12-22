0c75e0c4b6b18443d60ad6a124c8f2b2a8a91d86
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-21 17:12:52 -0800

    updates

--- apps/hello_world.py
+++ apps/hello_world.py
@@ -10,11 +10,22 @@ if __name__ == "__main__":
     print("To exit, press 'START' and 'SELECT' simultaneously.")
     start_pressed = False; select_pressed = False
     for button, state in get_controller_events():
-        print("Button: %s\tState: %d" % (button, state))
+        print("Button: %s\tState: %s" % (button, state))
+        sleep(1)
         if button == 'START':
+            print("IN IF START")
+            sleep(1)
             start_pressed = (state == 1)
+            print("START PRESSED UPDATE")
+            sleep(1)
         elif button == 'SELECT':
+            print("IN ELIF SELECT")
+            sleep(1)
             select_pressed = (state == 1)
+            print("SELECT PRESSED UPDATE")
+            sleep(1)
+        print("BEFORE EXIT CHECK")
+        sleep(1)
         if start_pressed and select_pressed:
             print("Exiting :-)")
             sleep(5)
