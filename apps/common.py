2b865ac9f9eb69eaec53e45efccec9e0b247d24c
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-22 09:14:46 -0800

    updates

--- apps/common.py
+++ apps/common.py
@@ -13,18 +13,6 @@ ROOT_PATH = Path('/')
 SCREEN_HEIGHT = 30
 SCREEN_WIDTH = 80
 
-# ANSI escape sequences: https://jakob-bagterp.github.io/colorist-for-python/ansi-escape-codes/standard-16-colors/#foreground-text-and-background-colors
-class ANSI:
-    BLACK =   '\033[90m'
-    RED =     '\033[91m'
-    GREEN =   '\033[92m'
-    YELLOW =  '\033[93m'
-    BLUE =    '\033[94m'
-    MAGENTA = '\033[95m'
-    CYAN =    '\033[96m'
-    WHITE =   '\033[97m'
-    ENDC =    '\033[0m'
-
 # import `inputs`: https://github.com/zeth/inputs
 try:
     from inputs import get_gamepad
@@ -57,13 +45,9 @@ INPUT_TO_R36S = {
 }
 R36S_TO_INPUT = {v:k for k,v in INPUT_TO_R36S.items()}
 
-# remove ANSI sequences from a string (e.g. to accurately calculate length): https://stackoverflow.com/a/14693789/2134991
-def remove_ansi(s):
-    return re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])').sub('', s)
-
 # add spaces to the left of a string to center it to a max length
 def pad_to_center(s, max_width=SCREEN_WIDTH):
-    visible_length = len(remove_ansi(s))
+    visible_length = len(s)
     if visible_length >= max_width:
         return s
     else:
@@ -92,9 +76,9 @@ def select_options_dialog(values, title=None, text=None, select_multi=False):
     # set up title + text
     lines = [''] # start with empty line to make sure we're at the start of a line for actual content
     if title is not None:
-        lines.append(pad_to_center(ANSI.MAGENTA + '= ' + title + ' =' + ANSI.ENDC))
+        lines.append(pad_to_center('= %s =' % title))
     if select_multi:
-        lines.append(pad_to_center(ANSI.YELLOW + 'Press START when selection is complete' + ANSI.ENDC))
+        lines.append(pad_to_center("Press START when selection is complete"))
     if text is not None:
         lines += [s.rstrip() for s in text.splitlines()]
     if title is not None or text is not None or select_multi:
@@ -102,7 +86,7 @@ def select_options_dialog(values, title=None, text=None, select_multi=False):
 
     # set up selection
     first_selectable_ind = len(lines)
-    values = [(None, ANSI.RED + "= Cancel =" + ANSI.ENDC)] + values
+    values = [(None, "= Cancel =")] + values
     return_values = [None] + [v for v,t in values]
     lines += [('   ( ) ' + t) for v,t in values]
     selection = set()
@@ -156,10 +140,10 @@ def select_file(curr_path=Path('~').resolve(), select_folder=False):
         text = "Current Directory: %s" % curr_path
         values = list()
         if select_folder and curr_path.is_dir():
-            values.append((curr_path.parent, ANSI.GREEN + '=== Select Current Directory ===' + ANSI.ENDC))
+            values.append((curr_path.parent, '=== Select Current Directory ==='))
         if curr_path != ROOT_PATH:
-            values.append((curr_path.parent, ANSI.BLUE + '..' + ANSI.ENDC))
-        values += sorted(((p, ANSI.BLUE + p.name + ANSI.ENDC if p.is_dir() else p.name) for p in curr_path.glob('*')), key=lambda x: x[0].name.lower())
+            values.append((curr_path.parent, '..'))
+        values += sorted(((p, p.name) for p in curr_path.glob('*')), key=lambda x: x[0].name.lower())
         result = select_options_dialog(title=title, text=text, values=values, select_multi=False)
         if result is None or result.is_file():
             return result
@@ -172,6 +156,6 @@ def select_file(curr_path=Path('~').resolve(), select_folder=False):
 def view_file_info(path):
     stat_result = path.stat()
     title = "File Information"
-    text = ANSI.BLUE + '- Path:' + ANSI.ENDC + (' %s' % path)
-    text += ('\n' + ANSI.BLUE + '- Size:' + ANSI.ENDC + (' %s bytes' % '{:,}'.format(stat_result.st_size)))
+    text = '- Path: %s' % path
+    text += '\n- Size: %s bytes' % '{:,}'.format(stat_result.st_size)
     message_dialog(title=title, text=text)
