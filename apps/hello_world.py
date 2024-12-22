6a02f3795926aa8b4a3877c0c2eb00cc1dacb87c
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-21 16:09:02 -0800

    updates

--- apps/hello_world.py
+++ apps/hello_world.py
@@ -4,14 +4,14 @@ Hello World app
 '''
 import sdl2
 import sdl2.ext
-sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO)
+#sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO)
 sdl2.SDL_Init(sdl2.SDL_INIT_JOYSTICK)
 joystick = sdl2.SDL_JoystickOpen(0)
-sdl2.ext.Window("test", size=(640,480),position=(0,0),flags=sdl2.SDL_WINDOW_SHOWN)
-window.refresh())
+#sdl2.ext.Window("test", size=(640,480),position=(0,0),flags=sdl2.SDL_WINDOW_SHOWN)
+#window.refresh())
 while True:
     for event in sdl2.ext.get_events():
         if event.type==sdl2.SDL_KEYDOWN:
-            print sdl2.SDL_GetKeyName(event.key.keysym.sym).lower()
+            print(sdl2.SDL_GetKeyName(event.key.keysym.sym).lower())
         elif event.type==sdl2.SDL_JOYAXISMOTION:
-            print [event.jaxis.axis,event.jaxis.value]
+            print(event.jaxis.axis,event.jaxis.value)
--- apps/hello_world.sh
+++ apps/hello_world.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 # execute Hello World app
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "HELLO WORLD START"
 python3 "$SCRIPT_DIR/hello_world.py"
+echo "HELLO WORLD END"
+sleep 5
