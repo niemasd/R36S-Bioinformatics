783bcd594829aa5645150fa41fc1dc2b8be0c89f
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-21 17:02:47 -0800

    updates

--- apps/common.py
+++ apps/common.py
@@ -9,13 +9,47 @@ from pathlib import Path
 # general constants
 ROOT_PATH = Path('/')
 
-# import prompt_toolkit
+# import `inputs`: https://github.com/zeth/inputs
 try:
-    from prompt_toolkit.formatted_text import HTML
-    from prompt_toolkit.shortcuts import checkboxlist_dialog, message_dialog, radiolist_dialog
+    from inputs import get_gamepad
 except:
-    raise RuntimeError("Missing Python package 'prompt_toolkit'. Try rerunning the R36S-Bioinformatics 'INSTALL' app.")
+    raise RuntimeError("Missing Python package 'inputs'. Try rerunning the R36S-Bioinformatics 'INSTALL' app.")
 
+# mapping from `inputs` gamepad to R36S controller
+INPUT_TO_R36S = {
+    'ABS_RX':              'RIGHTX',
+    'ABS_RY':              'RIGHTY',
+    'ABS_X':               'LEFTX',
+    'ABS_Y':               'LEFTY',
+    'BTN_DPAD_DOWN':       'DOWN',
+    'BTN_DPAD_LEFT':       'LEFT',
+    'BTN_DPAD_RIGHT':      'RIGHT',
+    'BTN_DPAD_UP':         'UP',
+    'BTN_EAST':            'A',
+    'BTN_NORTH':           'X',
+    'BTN_SOUTH':           'B',
+    'BTN_TL':              'L1',
+    'BTN_TL2':             'L2',
+    'BTN_TR':              'R1',
+    'BTN_TR2':             'R2',
+    'BTN_TRIGGER_HAPPY_1': 'SELECT',
+    'BTN_TRIGGER_HAPPY_2': 'START',
+    'BTN_TRIGGER_HAPPY_3': 'L3',
+    'BTN_TRIGGER_HAPPY_4': 'R3',
+    'BTN_TRIGGER_HAPPY_5': 'FN',
+    'BTN_WEST':            'Y',
+}
+R36S_TO_INPUT = {v:k for k,v in INPUT_TO_R36S.items()}
+
+# generator to yield each controller event as (button, state) tuples
+# button states: 0 = unpressed; 1 = pressed
+# joystick states: 0 = neutral; <0 = left (X) or up (Y); >0 = right (X) or down (Y)
+def get_controller_events():
+    while True:
+        for event in get_gamepad():
+            yield (INPUT_TO_R36S[event.code], event.state)
+
+'''
 # file selector
 def select_file(curr_path=Path('~').resolve(), select_folder=False):
     title = "Select File/Folder"
@@ -43,3 +77,4 @@ def view_file_info(path):
     text += '\n- <ansiblue>Size:</ansiblue> %s bytes' % '{:,}'.format(stat_result.st_size)
     text = HTML(text)
     message_dialog(title=title, text=text).run()
+'''
--- apps/hello_world.py
+++ apps/hello_world.py
@@ -2,22 +2,17 @@
 '''
 Hello World app
 '''
-# https://github.com/zeth/inputs
-from inputs import get_gamepad
+from common import get_controller_events
 if __name__ == "__main__":
     print("Welcome to R36S-Bioinformatics!")
     print("This 'Hello, World!' app will print your button inputs.")
     print("To exit, press 'START' and 'SELECT' simultaneously.")
-    start_pressed = False
-    select_pressed = False
-    while True:
-        events = get_gamepad()
-        for event in events:
-            print(event.ev_type, event.code, event.state)
-            if event.ev_type == 'Key':
-                if event.code == 'BTN_TRIGGER_HAPPY2':
-                    start_pressed = (event.state == 1)
-                elif event.code == 'BTN_TRIGGER_HAPPY1':
-                    select_pressed = (event.state == 1)
-            if start_pressed and select_pressed:
-                exit()
+    start_pressed = False; select_pressed = False
+    for button, state in get_controller_events():
+        print("Button: %s\tState: %d" % (button, state))
+        if button == 'START':
+            start_pressed = (state == 1)
+        elif button == 'SELECT':
+            select_pressed = (state == 1)
+        if start_pressed and select_pressed:
+            exit()
