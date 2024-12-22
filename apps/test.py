342df397af651a84b5e03b5334fcba58e5c7de3c
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-22 15:58:52 -0800

    updates

--- apps/test.py
+++ apps/test.py
@@ -2,7 +2,7 @@
 from common import clear_screen, text_input_dialog
 from time import sleep
 if __name__ == "__main__":
-    s = text_input_dialog(title="Test Input Title", text="Test Input Text", curr_string="Niema")
+    s = text_input_dialog(title="Test Input Title", text="Test Input Text")
     clear_screen()
     print("Here's your text:\n=================\n%s" % s)
     sleep(5)
