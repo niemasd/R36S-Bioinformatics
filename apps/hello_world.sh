f370cced540c5dee617051311b73a0579f23985c
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-21 16:30:28 -0800

    updates

--- apps/hello_world.sh
+++ apps/hello_world.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 # execute Hello World app
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-echo "HELLO WORLD START"
 python3 "$SCRIPT_DIR/hello_world.py"
-echo "HELLO WORLD END"
-sleep 5
