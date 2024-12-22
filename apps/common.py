2dd3c9878703b7bed31b9a745b7fe6726042c987
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-21 17:57:02 -0800

    updates

--- apps/common.py
+++ apps/common.py
@@ -56,7 +56,7 @@ def get_controller_events():
 
 # file selector TODO FIX
 def select_file(curr_path=Path('~').resolve(), select_folder=False):
-    print('\n'.join('0'*SCREEN_WIDTH for _ in range(SCREEN_HEIGHT)), end='')
+    print('\n'.join(('%d' % (i%10)) * SCREEN_WIDTH for i in range(SCREEN_HEIGHT)), end='')
     sleep(5)
     title = "Select File/Folder"
     while True:
