6f9cdeade37dd272463895be71a8dab238cc71f4
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-22 15:49:09 -0800

    updates

--- apps/test.py
+++ apps/test.py
@@ -2,7 +2,6 @@
 from common import text_input_dialog
 from time import sleep
 if __name__ == "__main__":
-    print("STARTING DIALOG\n\n\n")
     s = text_input_dialog(title="Test Input Title", text="Test Input Text", curr_string="Niema")
-    #print("\n\n\nRETURN: " + s)
-    #sleep(2)
+    print_lines("Here's your text:\n=================\n%s" % s)
+    sleep(5)
