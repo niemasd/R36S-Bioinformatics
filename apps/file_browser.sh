7b6e6c20af6e2f744bc71db839c19600c6d2b6ae
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-21 18:03:44 -0800

    updates

--- apps/file_browser.sh
+++ apps/file_browser.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 # execute file browser app
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-if ! python3 "$SCRIPT_DIR/file_browser.py" ; then
-    sleep 5
-fi
+python3 "$SCRIPT_DIR/file_browser.py"
--- apps/hello_world.sh
+++ apps/hello_world.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 # execute Hello World app
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-if ! python3 "$SCRIPT_DIR/hello_world.py" ; then
-    sleep 5
-fi
+python3 "$SCRIPT_DIR/hello_world.py"
