4c4551f75cc58317d54d21383eba6b20634f13bd
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-21 16:18:35 -0800

    updates

--- apps/hello_world.py
+++ apps/hello_world.py
@@ -2,16 +2,11 @@
 '''
 Hello World app
 '''
-import sdl2
-import sdl2.ext
-#sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO)
-sdl2.SDL_Init(sdl2.SDL_INIT_JOYSTICK)
-joystick = sdl2.SDL_JoystickOpen(0)
-#sdl2.ext.Window("test", size=(640,480),position=(0,0),flags=sdl2.SDL_WINDOW_SHOWN)
-#window.refresh())
-while True:
-    for event in sdl2.ext.get_events():
-        if event.type==sdl2.SDL_KEYDOWN:
-            print(sdl2.SDL_GetKeyName(event.key.keysym.sym).lower())
-        elif event.type==sdl2.SDL_JOYAXISMOTION:
-            print(event.jaxis.axis,event.jaxis.value)
+from inputs import get_gamepad
+def main():
+    while True:
+        events = get_gamepad()
+        for event in events:
+            print(event.ev_type, event.code, event.state)
+if __name__ == "__main__":
+    main()
