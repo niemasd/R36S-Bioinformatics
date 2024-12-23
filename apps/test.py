a74c80c015186d8778aed6d5fb5769eb4e52ae80
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-22 16:04:11 -0800

    Removed test script

--- apps/test.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python3
-from common import clear_screen, text_input_dialog
-from time import sleep
-if __name__ == "__main__":
-    s = text_input_dialog(title="Test Input Title", text="Test Input Text")
-    clear_screen()
-    print("Here's your text:\n=================\n%s" % s)
-    sleep(5)
--- apps/test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# test app
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-python3 "$SCRIPT_DIR/test.py"
