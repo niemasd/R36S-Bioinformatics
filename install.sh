7cd09fe89f67315c62c53bf7501d9bad0fcd3756
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-22 16:08:25 -0800

    updates

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 VERSION='0.0.1'
 ES_SYSTEMS_CFG_PATH='/etc/emulationstation/es_systems.cfg'
 ES_SYSTEMS_CFG_BACKUP_PATH="$ES_SYSTEMS_CFG_PATH.r36s-bioinformatics.bak"
-DEPS_LINUX='cmake g++ git libc6-dev libstdc++-9-dev linux-libc-dev make ninja-build python3 python3-pip'
+DEPS_LINUX='cmake g++ git libc6-dev libstdc++-9-dev linux-libc-dev make ninja-build python3 python3-pip wget'
 DEPS_PYTHON='inputs'
 ES_SYSTEMS_CFG_BIOINFORMATICS_SYSTEM_ENTRY='\t<system>\n\t\t<name>Bioinformatics</name>\n\t\t<fullname>Bioinformatics</fullname>\n\t\t<path>{roms_dir}/bioinformatics/</path>\n\t\t<extension>.sh .SH</extension>\n\t\t<command>sudo chmod 666 /dev/tty1; %ROM% 2>&1 > /dev/tty1; printf "\\033c" >> /dev/tty1</command>\n\t\t<platform>bioinformatics</platform>\n\t\t<theme>bioinformatics</theme>\n\t</system>'
 
@@ -17,6 +17,15 @@ echo "Installing Linux dependencies..."
 (sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y --reinstall $DEPS_LINUX && sudo apt-get autoremove && sudo apt-get clean) || (echo "Failed to install Linux dependencies" && sleep 5 && exit 1)
 echo "Installing Python dependencies..."
 (python3 -m pip install --upgrade --no-cache-dir $DEPS_PYTHON) || (echo "Failed to install Python dependencies" && sleep 5 && exit 1)
+
+# install Minimap2
+wget -qO- "https://github.com/lh3/minimap2/archive/refs/tags/v2.28.tar.gz" | tar -zx
+cd minimap2-*
+make
+chmod a+x minimap2
+sudo mv minimap2 /usr/bin/minimap2
+cd ..
+rm -rf minimap2-*
 
 # find roms path
 echo "Finding roms path..."
