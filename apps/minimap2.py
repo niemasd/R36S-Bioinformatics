ba9766beef87d2d76fdf6ebd5b765a45f8233519
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-23 14:05:10 -0800

    updates

--- apps/minimap2.py
+++ apps/minimap2.py
@@ -5,7 +5,6 @@ Minimap2 app
 from common import clear_screen, message_dialog, select_file, select_options_dialog
 from os import system
 from pathlib import Path
-from time import sleep # TODO DELETE
 VERSION = '1.0.0'
 if __name__ == "__main__":
     # set things up
@@ -16,43 +15,48 @@ if __name__ == "__main__":
     out_prefix = None
     minimap2_preset = None
 
-    # set up text
-    text = ''
-    text += "\n- Reference Genome: %s" % ref_path
-    text += "\n- Reads: %s" % (None if len(reads_paths) == 0 else ('\n' + '\n'.join("  - %s" % r for r in reads_paths)))
-    text += "\n- Output File: %s" % (None if out_folder is None else '%s/%s.bam' % (out_folder, out_prefix))
-    text += "\n- Minimap2 Preset: %s" % minimap2_preset
+    # define values
+    values = [
+        ('run', "Run Minimap2"),
+        ('ref', "Select reference genome FASTA"),
+        ('reads', "Select reads FASTQ(s)"),
+        ('out_folder', "Select output folder"),
+        ('out_prefix', "Select output file prefix"),
+        ('preset', "Select Minimap2 preset"),
+        (None, "Quit"),
+    ]
 
     # app loop
     while True:
-        values = [
-            ('run', "Run Minimap2"),
-            ('ref', "Select reference genome FASTA"),
-            ('reads', "Select reads FASTQ(s)"),
-            ('out_folder', "Select output folder"),
-            ('out_prefix', "Select output file prefix"),
-            ('preset', "Select Minimap2 preset"),
-            (None, "Quit"),
-        ]
+        # select option
+        text = ''
+        text += "\n- Reference Genome: %s" % ref_path
+        text += "\n- Reads: %s" % (None if len(reads_paths) == 0 else ('\n' + '\n'.join("  - %s" % r for r in reads_paths)))
+        text += "\n- Output File: %s" % (None if out_folder is None else '%s/%s.bam' % (out_folder, out_prefix))
+        text += "\n- Minimap2 Preset: %s" % minimap2_preset
         result = select_options_dialog(title=title, text=text, values=values)
+
+        # quit
         if result is None:
             break
+
+        # select reference genome
         elif result == 'ref':
             tmp = select_file(title="Select Reference Genome FASTA", curr_path=Path('/'))
             if tmp is not None:
                 ref_path = tmp
+
+        # select reads
         elif result == 'reads':
-            try:
-                reads_dir_path = select_file(title="Select path containing Reads FASTQ(s)", curr_path=Path('/'), select_folder=True)
-                if reads_dir_path is None:
-                    continue
-                tmp = select_file(title="Select Reads FASTQ(s)", curr_path=reads_dir_path, select_multi=True)
-                if tmp is None or len(tmp) == 0:
-                    continue
-                reads_paths = sorted(tmp)
-            except Exception as e:
-                print('\n\n\n%s' % str(e))
-                sleep(2)
+            reads_dir_path = select_file(title="Select path containing Reads FASTQ(s)", curr_path=Path('/'), select_folder=True)
+            if reads_dir_path is None:
+                continue
+            tmp = select_file(title="Select Reads FASTQ(s)", curr_path=reads_dir_path, select_multi=True)
+            if tmp is None or len(tmp) == 0:
+                continue
+            reads_paths = sorted(tmp)
+
+        # run Minimap2
         elif result == "run":
             if ref_path is None:
                 message_dialog(title="ERROR", text="Must select a reference genome")
