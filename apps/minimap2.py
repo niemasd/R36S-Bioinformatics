463084839ad7d520dcc53b999adc6d5f2cb0e5c7
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-23 12:34:20 -0800

    updates

--- apps/minimap2.py
+++ apps/minimap2.py
@@ -2,7 +2,7 @@
 '''
 Minimap2 app
 '''
-from common import select_file
+from common import select_file, select_options_dialog
 from pathlib import Path
 VERSION = '1.0.0'
 if __name__ == "__main__":
@@ -15,4 +15,6 @@ if __name__ == "__main__":
         values = [
             (None, "Quit"),
         ]
-        select_options_dialog(title=title, values=values)
+        result = select_options_dialog(title=title, values=values)
+        if result is None:
+            break
